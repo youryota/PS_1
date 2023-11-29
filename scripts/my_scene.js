a1978f0624a4c72b5a4a3cc85b9daaa70e7bc37d
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2023-11-29 14:18:11 +0900

    1-4

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -1,43 +1,40 @@
-
-// MyScene1クラス
-// 他のJSファイルから呼び出された場合はシーンを返す
 class MyScene extends Phaser.Scene {
-
-    // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
     constructor() {
         super({ key: 'MyScene', active: true });
     }
 
-    // シーンの事前読み込み処理
     preload() {
-         // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
     }
 
-    // シーン初期化処理
     create() {
-         // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
-        this.player = this.add.image(D_WIDTH/2, D_HEIGHT/2, 'taro');
+        this.taro = this.add.image(D_WIDTH/2, D_HEIGHT/2, 'taro');
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
-        this.player_direction = 1 ;
-
+        this.taro.angle = 0; // taroの角度を初期化
+        this.player_direction = 1;
     }
 
-  // 毎フレーム実行される繰り返し処理
     update() {
-        if (this.player.x >= D_WIDTH -230 || this.player.y >= D_HEIGHT) this.player_direction = -1;
-        if (this.player.x <= 0 || this.player.y <= 0) this.player_direction = 1;
+        if (this.taro.x >= D_WIDTH - 230 || this.taro.y >= D_HEIGHT) this.player_direction = -1;
+        if (this.taro.x <= 0 || this.taro.y <= 0) this.player_direction = 1;
 
-        if(this.player_direction == 1){
-            this.player.y += 5;
-            this.player.x += 5;
-        }else{
-            this.player.y -= 5;
-            this.player.x -= 5;
+        if (this.player_direction == 1) {
+            this.taro.y += 5;
+            this.taro.x += 5;
+        } else {
+            this.taro.y -= 5;
+            this.taro.x -= 5;
         }
 
-    }
+        this.taro.angle += 5; // 角度を5度ずつ増やす
 
+        // 45度方向に移動
+        const radian = Phaser.Math.DegToRad(this.taro.angle); // 角度をラジアンに変換
+        const moveX = 5 * Math.cos(radian); // X方向の移動量
+        const moveY = 5 * Math.sin(radian); // Y方向の移動量
+        this.taro.x += moveX;
+        this.taro.y += moveY;
+    }
 }
