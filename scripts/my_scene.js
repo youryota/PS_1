da3ca05385505d0a6789948a8d9f777157b8d4f3
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2023-11-29 14:59:21 +0900

    1-5

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -1,53 +1,76 @@
+
 // MyScene1クラス
 // 他のJSファイルから呼び出された場合はシーンを返す
 class MyScene extends Phaser.Scene {
 
-    // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
-    constructor() {
-        super({ key: 'MyScene', active: true });
-    }
-
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('background', 'assets/background.png');
+        this.load.image('sky', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
+        this.load.image('jori', 'assets/jori.png');
     }
+
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
-        this.player = this.add.image(D_WIDTH/2, D_HEIGHT/2, 'taro');
-        this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
-        this.player.angle = 0;
-        this.player_direction = 1 ;
-        
-
+        this.add.image(400, 300, 'sky');
+        // const player = this.physics.add.sprite(400, 300, 'taro');
+        // this.player = player
+        // this.player.angle = 0;
+        // this.player_direction = 1;
+        const player = this.physics.add.sprite(400, 300, 'taro');
+        this.player = player
+        const player1 = this.physics.add.sprite(350, 300, 'jori');
+        this.player1 = player1
     }
 
+    arrow_move(cursors, object){
+    
+        if(cursors.left.isDown){
+            console.log("Left");
+            object.setVelocityX(-200);
+    
+    
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            object.setVelocityX(200);
+    
+        }else{
+            object.setVelocity(0,0);
+        }
+    }
+
+    arrow_move2(cursors, object){
+    
+        if(cursors.left.isDown){
+            console.log("Left");
+            object.setVelocityX(200);
+    
+    
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            object.setVelocityX(-200);
+    
+        }else{
+            object.setVelocity(0,0);
+        }
+    }
+    
   // 毎フレーム実行される繰り返し処理
     update() {
-        if (this.player.x >= D_WIDTH -230 || this.player.y >= D_HEIGHT) this.player_direction = -1;
-        if (this.player.x <= 0 || this.player.y <= 0) this.player_direction = 1;
+        // if (this.player.x >= D_WIDTH -230 || this.player.y >= D_HEIGHT) this.player_direction = -1;
+        // if (this.player.x <= 0 || this.player.y <= 0) this.player_direction = 1;
 
-        if(this.player_direction == 1){
-            this.player.y += 5;
-            this.player.x += 5;
-            // 回転角度を更新
-             this.player.angle += 1;
-            // 回転角度を設定
-            this.player.setAngle( this.player.angle );
-        }else{
-            // 回転角度を更新
-            this.player.angle += 1;
-            // 回転角度を設定
-            this.player.setAngle( this.player.angle );
-            this.player.y -= 5;
-            this.player.x -= 5;
-        }
-
-
-
+        // if(this.player_direction == 1){
+        //     this.player.setVelocity(100, -100);
+        //     this.player.angle += 5;
+        //     this.player.setAngle( this.player.angle );
+        // }
+        let cursors = this.input.keyboard.createCursorKeys();
+        this.arrow_move(cursors, this.player);
+        this.arrow_move2(cursors, this.player1);
     }
+
 }
